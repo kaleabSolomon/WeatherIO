d4945f322e444daf74eb038ac2870264ebd0305b
Kaleab Solomon <kaleabslmn@gmail.com>
2024-02-09 23:08:39 +0300

    add lightmode config

---
+++ lib/theme/theme.dart
@@ -0,0 +1,9 @@
+import "package:flutter/material.dart";
+
+ThemeData lightMode = ThemeData(
+    brightness: Brightness.light,
+    colorScheme: const ColorScheme.light(
+      background: Color.fromARGB(255, 220, 235, 255),
+      primary: Color.fromARGB(255, 13, 60, 99),
+      secondary: Colors.white,
+    ));
