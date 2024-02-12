38c453109e6511aa8e44be2f43ac1bc0ebf16949
Kaleab Solomon <kaleabslmn@gmail.com>
2024-02-13 00:29:50 +0300

    alternate between light and dark svgs when changing themes

--- lib/widgets/weather_card.dart
+++ lib/widgets/weather_card.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_io/theme/theme.dart';
+import 'package:weather_io/theme/theme_provider.dart';
 
 class WeatherCard extends StatelessWidget {
   const WeatherCard({super.key});
@@ -6,65 +10,74 @@ class WeatherCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 220,
-      margin: const EdgeInsets.symmetric(horizontal: 12),
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      width: MediaQuery.of(context).size.width * 0.92,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        color: Color.fromARGB(255, 122, 212, 254),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Addis Abeba",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Theme.of(context).colorScheme.secondary,
-                    ),
-                  ),
-                  Text(
-                    "Saturday, 01 may 2021",
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Theme.of(context).colorScheme.secondary,
-                    ),
-                  )
-                ],
-              ),
-              Text(
-                "24°",
-                style: TextStyle(
-                  fontSize: 42,
-                  fontWeight: FontWeight.bold,
-                  color: Theme.of(context).colorScheme.secondary,
-                ),
-              ),
-              Text(
-                "Sunny",
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Theme.of(context).colorScheme.secondary,
-                ),
-              )
-            ],
+        height: 220,
+        margin: const EdgeInsets.symmetric(horizontal: 12),
+        width: MediaQuery.of(context).size.width * 0.92,
+        child: Stack(children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: SvgPicture.asset(
+              Provider.of<ThemeProvider>(context).themeData == lightMode
+                  ? "assets/svg/pattern-lightmode.svg"
+                  : "assets/svg/pattern-darkmode.svg",
+              alignment: Alignment.center,
+              fit: BoxFit.cover,
+            ),
           ),
-          const Icon(
-            Icons.sunny,
-            size: 80,
-            color: Colors.orange,
-          )
-        ],
-      ),
-    );
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Addis Abeba",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).colorScheme.secondary,
+                          ),
+                        ),
+                        Text(
+                          "Saturday, 01 may 2021",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Theme.of(context).colorScheme.secondary,
+                          ),
+                        )
+                      ],
+                    ),
+                    Text(
+                      "24°",
+                      style: TextStyle(
+                        fontSize: 42,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                    ),
+                    Text(
+                      "Sunny",
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: Theme.of(context).colorScheme.secondary,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+                const Icon(
+                  Icons.sunny,
+                  size: 90,
+                  color: Colors.orange,
+                )
+              ],
+            ),
+          ),
+        ]));
   }
 }
