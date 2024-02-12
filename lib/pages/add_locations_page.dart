27db8e378b1e3ef4f6fe02a7a1fcc3f27f2535c9
Kaleab Solomon <kaleabslmn@gmail.com>
2024-02-13 02:10:56 +0300

    add button to add a city to the page

--- lib/pages/add_locations_page.dart
+++ lib/pages/add_locations_page.dart
@@ -20,10 +20,39 @@ class _AddLocationsPageState extends State<AddLocationsPage> {
     return Scaffold(
       appBar: MyAppBar(
         height: MediaQuery.of(context).size.height * 0.15,
-        title: "Add Locations",
+        title: "City",
         toggleTheme: toggleTheme,
         appBarActions: [],
       ),
+      body: Column(children: [
+        Expanded(
+            child: Container(
+          color: Colors.blue,
+        )),
+        Container(
+          margin: const EdgeInsets.all(10),
+          width: MediaQuery.of(context).size.width * 0.9,
+          child: TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(
+              backgroundColor: Color.fromARGB(255, 0, 34, 85),
+              padding:
+                  const EdgeInsets.symmetric(vertical: 18), // Button padding
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8), // Button border radius
+              ),
+            ),
+            child: const Text(
+              "Add Location",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+        )
+      ]),
     );
   }
 }
