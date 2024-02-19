b03793226b306bbbbb754f58fe6ef2a2504f08cc
Kaleab Solomon <kaleabslmn@gmail.com>
2024-02-19 23:58:51 +0300

    display message on futureforecast widget if no forecast detected

--- lib/widgets/future_forecast.dart
+++ lib/widgets/future_forecast.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 
 class FutureForecast extends StatelessWidget {
-  final String day;
-  final String condition;
-  final String maxTemp;
-  final String minTemp;
+  final String? day;
+  final String? condition;
+  final String? maxTemp;
+  final String? minTemp;
+  final bool? isNoForecast;
 
   const FutureForecast(
       {super.key,
-      required this.day,
-      required this.condition,
-      required this.maxTemp,
-      required this.minTemp});
+      this.day,
+      this.condition,
+      this.maxTemp,
+      this.minTemp,
+      this.isNoForecast});
 
   @override
   Widget build(BuildContext context) {
@@ -20,26 +22,35 @@ class FutureForecast extends StatelessWidget {
       height: 40,
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 18),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(day,
-                style: const TextStyle(
-                  color: Color.fromARGB(255, 103, 103, 103),
-                  fontSize: 20,
-                )),
-            const Icon(
-              Icons.cloudy_snowing,
-              size: 40,
-              color: Color.fromARGB(255, 190, 189, 189),
-            ),
-            Text("$maxTemp/$minTemp",
-                style: const TextStyle(
-                  color: Color.fromARGB(255, 103, 103, 103),
-                  fontSize: 18,
-                ))
-          ],
-        ),
+        child: isNoForecast == true
+            ? Center(
+                child: Text(
+                "No Forecast Available",
+                style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.primary),
+              ))
+            : Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(day ?? "",
+                      style: const TextStyle(
+                        color: Color.fromARGB(255, 103, 103, 103),
+                        fontSize: 20,
+                      )),
+                  const Icon(
+                    Icons.cloudy_snowing,
+                    size: 40,
+                    color: Color.fromARGB(255, 190, 189, 189),
+                  ),
+                  Text("$maxTemp/$minTemp",
+                      style: const TextStyle(
+                        color: Color.fromARGB(255, 103, 103, 103),
+                        fontSize: 18,
+                      ))
+                ],
+              ),
       ),
     );
   }
